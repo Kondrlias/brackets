a49bbcf1575eba19523563efe597e5c935994248
Julia Kondratsiuk <kondrlias@gmail.com>
2025-09-03 16:45:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const brackets = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i += 1) {
+    const lastElement = stack[stack.length - 1];
+
+    if (brackets.has(str[i])) {
+      if (str[i] === brackets.get(str[i]) && lastElement === str[i]) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else if (!stack.length || brackets.get(stack.pop()) !== str[i]) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 };
